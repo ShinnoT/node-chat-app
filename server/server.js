cf9e1cf0f53ae5f3548398faacbe3dd615ae1ef6
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-01-12 18:51:28 +0900

    port 3000 listener with express configured

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+app.use(express.static(publicPath));
+
+// app.get('/', (request, response) => {
+//   response.sendFile(publicPath);
+// });
+
+
+app.listen(3000, () => {
+  console.log('server up on port 3000');
+});
