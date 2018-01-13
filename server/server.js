9fc7694adf100aa01cf20b9f2bdad134d9bff3c3
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-01-13 15:35:00 +0900

    added connect and disconnet event handlers for websockets

--- server/server.js
+++ server/server.js
@@ -1,18 +1,46 @@
+// built in node modules thus no need to npm install
 const path = require('path');
+const http = require('http');
+
+// external modules
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-
 const app = express();
-app.use(express.static(publicPath));
 
+
+// const server = http.createServer((request, response) => {
+
+// });
+// however because express and http modules are highly integrated we can just do:
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+
+app.use(express.static(publicPath));
 // app.get('/', (request, response) => {
 //   response.sendFile(publicPath);
 // });
 
+//io.on is an eventListener meaning waiting for something to happen
+//in this case waiting for a connection to the server
+io.on('connection', (socket) => {
+  console.log('new user connected');
 
-app.listen(port, () => {
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+// since now we are using http server instead of express app server we need to do:
+// app.listen(port, () => {
+//   console.log(`server up on port ${port}`);
+// });
+server.listen(port, () => {
   console.log(`server up on port ${port}`);
 });
