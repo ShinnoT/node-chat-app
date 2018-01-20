e0a892fb0855d1adf2e68a9d496af168c8ebb053
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-01-21 00:08:31 +0900

    test note

--- playground/time.js
+++ playground/time.js
@@ -1,23 +1,22 @@
 // let date = new Date();
 // console.log(date.getMonth());
 
-let moment = require('moment');
+let moment = require("moment");
 
 let date = moment();
 // date.add(1, 'year').subtract(1, 'months');
-console.log(date.format('MMM Do, YYYY'));
+console.log(date.format("MMM Do, YYYY"));
 
+console.log(date.format("h:m a"));
 
-console.log(date.format('h:m a'));
-
-
-console.log('------------------');
-let createdAt = new Date('January 10, 2006 03:24:00').getTime();
+console.log("------------------");
+let createdAt = new Date("January 10, 2006 03:24:00").getTime();
 let newDate = moment(createdAt);
-console.log(newDate.format('MMM Do, YYYY'));
+console.log(newDate.format("MMM Do, YYYY"));
 
-console.log(newDate.format('h:m a'));
+console.log(newDate.format("h:m a"));
 
-
-console.log('---------------');
+console.log("---------------");
 console.log(moment().valueOf() === new Date().getTime());
+
+//test note
